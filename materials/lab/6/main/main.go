c1ce7090c7399407554b745440da99ea383ff9f9
Zachary Crimmel <70920329+ZuhChar@users.noreply.github.com>
2022-05-08 23:10:48 -0600

    Update main.go

--- materials/lab/6/main/main.go
+++ materials/lab/6/main/main.go
@@ -13,13 +13,15 @@ import (
 //TODO_1: When LOG_LEVEL = 0 DO NOT LOG anything
 //TODO_1: When LOG_LEVEL = 1 LOG API details only 
 //TODO_1: When LOG_LEVEL = 2 LOG API details and file matches (e.g., everything)
+LOG_LEVEL := 0
 
 func main() {
-	
-	log.Println("starting API server")
-	//create a new router
-	router := mux.NewRouter()
-	log.Println("creating routes")
+	if LOG_LEVEL == 1 || LOG_LEVEL == 2{
+		log.Println("starting API server")
+		//create a new router
+		router := mux.NewRouter()
+		log.Println("creating routes")
+	}
 	//specify endpoints
 	router.HandleFunc("/", scrape.MainPage).Methods("GET")
 
@@ -28,8 +30,11 @@ func main() {
 	router.HandleFunc("/indexer", scrape.IndexFiles).Methods("GET")
 	router.HandleFunc("/search", scrape.FindFile).Methods("GET")		
     //TODO_2 router.HandleFunc("/addsearch/{regex}", scrape.TODOREPLACE).Methods("GET")
+	router.HandleFunc("/addsearch/{regex}", scrape.AddRegex).Methods("GET")
     //TODO_3 router.HandleFunc("/clear", scrape.TODOREPLACE).Methods("GET")
+	router.HandleFunc("/clear", scrape.ClearRegex).Methods("GET")
     //TODO_4 router.HandleFunc("/reset", scrape.TODOREPLACE).Methods("GET")
+	router.HandleFunc("/reset", scrape.ResetRegex).Methods("GET")
 
 
 
